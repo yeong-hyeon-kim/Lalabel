11f1f6c11f5db990997528375232ee75e21698c1
INA <codechemi@gmail.com>
2023-01-09 21:38:40 +0900

    Init!🎉

---
+++ README.md
@@ -0,0 +1,50 @@
+# 📕 라라벨(Lalabel)
+
+- `Github` `Lable` 템플릿을 작성합니다.
+
+## 🏷️ 기능(Function)
+
+- 작성된 `Label` 템플릿을 다른 레포지토리에서 사용합니다.
+
+## 💡 사용 예제(Usage Example)
+
+코드와 설명을 통해 사용 방법을 자세히 설명합니다.
+
+- 사용 방법
+
+## 💻 개발 환경(Develop Environment)
+
+### 🧰 시스템 환경(System Environment)
+
+||운영체제(OS)|언어(Language)|종속성(Dependency)|
+|-|:-:|:-:|:-:|
+|명칭(Name)|![Windows](https://img.shields.io/badge/Windows-0078D6?style=flat-square&logo=Windows&logoColor=white)|![PYTHON](https://img.shields.io/badge/PYTHON-3776AB?style=flat-square&logo=Python&logoColor=white)|![PY-PI](https://img.shields.io/badge/PYPI-3775A9?style=flat-square&logo=PyPI&logoColor=white)|
+|버전(Version)|`11`|`3.11`|[requirements](./requirements.txt)|
+
+### 🗂️ 라이브러리(Libraries)
+
+- Python 패키지 종속성
+
+``` bash
+pip freeze > requirements.txt
+```
+
+- Python 패키지 복원
+
+``` bash
+pip install -r requirements.txt
+```
+
+## 🔍 정보(Information)
+
+### 🖋️ 저자(Author)
+
+- [Yeonghyeon Kim](https://github.com/yeong-hyeon-kim/) – codechemi@gmail.com
+
+### ⚖️ 라이센스(License)
+
+- 다음 라이센스를 준수하며 [License](./License)에서 자세한 정보를 확인할 수 있습니다.
+
+## 📖 비고(Remark)
+
+- 비고를 작성합니다.
---
+++ script.py
@@ -0,0 +1,63 @@
+from github import Github
+import json
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
+
+Token = ""
+RepositoryName = ""
+
+# 레이블 객체
+Repository = [{"Repository": RepositoryName}]
+Labels = []
+
+# 레포지토리 정보 가져오기
+def GithubRepository(access_token, repository_name):
+    githubObject = Github(access_token)
+    repository = githubObject.get_user().get_repo(repository_name)
+    return repository
+
+# 레포지토리 레이블 가져오기
+def PatchLabel():
+    repository = GithubRepository(Token, RepositoryName)
+    labels = repository.get_labels()
+    for label in labels:
+        Labels.append({"name": label.name, "color": label.color,
+                      "description": label.description, "hex": "#" + label.color})
+    Repository.append(Labels)
+
+    with open('labels.json', 'w', encoding='UTF-8-sig') as f:
+        f.write(json.dumps(Repository, ensure_ascii=False))
+
+# 레포지토리 레이블 제거
+def DeleteDefaultLabel():
+    repository = GithubRepository(Token, RepositoryName)
+    labels = repository.get_labels()
+    # 기본 레이블만 제거
+    for label in labels:
+        if label.raw_data["default"] == True:
+            label.delete()
+        else:
+            label.delete()
+
+# 레포지토리 레이블 생성
+def CreateLabel():
+    Labels = []
+    repository = GithubRepository(Token, RepositoryName)
+
+    with open('labels.json', 'r', encoding='UTF-8-sig') as file:
+        Labels = json.load(file)
+
+    for label in Labels[1]:
+        repository.create_label(
+            label["name"], label["color"], label["description"])
+        print(label)
+
+# Github Action
+if __name__ == "__main__":
+    access_token = os.environ['TOKEN']
+
+PatchLabel()
+DeleteDefaultLabel()
+# CreateLabel()
